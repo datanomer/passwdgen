ee746c02e874232f34c4d623b6b2d5fe60fbb9cd
datanomer <niccsacc@protonmail.com>
2023-08-27 13:05:58 +0300

    First working version

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 #include <string.h>
 #include <random>
 #include <list>
+#include <cstdlib>
 
 struct characters
 {
- 
-    char letter[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','x','y','z','w','v'};   
-    char spclchar[14] = {'!','"','#','$','%','&','(',')','*','+',',','-','.','/',};
-    char num[10] = {'0','1','2','3','4','5','6','7','8','9'};
-   
+    char chr[76] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','X','Y','Z','W','V','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','x','y','z','w','v','!','"','#','$','%','&','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9'};   
+    //char spclchar[14] = {'!','"','#','$','%','&','(',')','*','+',',','-','.','/',};
+    //char num[10] = {'0','1','2','3','4','5','6','7','8','9'};
+    // Todo:
+    // * Options for special characters and numbers
+
 } characters;
 
 
@@ -17,13 +19,15 @@ void passgen(int input)
 {
     std::random_device rd;
     std::mt19937 rng(rd());
-    std::uniform_int_distribution<int> uni(0,50);
-    auto rando = uni(rng);
+    std::uniform_int_distribution<int> uni(0,76);
+    int rando = uni(rng);
 
-    for (int i = 0; i < input; i++) {
-        std::cout << characters.letter[rando];
+        for (int i = 0; i < input; i++) 
+        {
+            std::cout << characters.chr[std::rand()%rando];
+            
+        }
         
-    }
 }
 
 
